734bedf4c31493309f1d449f92273eb6c4c65efe
MathieuCoding <polletmathieu.pro@gmail.com>
2023-02-07 11:34:22 +0100

    edit working

--- script.js
+++ script.js
@@ -1,63 +1,57 @@
-const pushBtn = document.querySelector('#push');
-const newTaskInput = document.querySelector('#newtask input');
-const tasks = document.querySelector('#tasks');
-const edit = document.querySelector('#edit');
+window.addEventListener('load', () => {
+
+    const pushBtn = document.querySelector('#push');
+    const newTaskInput = document.querySelector('#newtask input');
+    const tasks = document.querySelector('#tasks');
 
 
-function addNewTask()
-{
-    if(newTaskInput.value.length == 0)
-    {
-        alert("Please enter a task's name");
-    }else{
-        const task = document.createElement('div');
-        task.classList.add('task');
-        tasks.appendChild(task);
+    pushBtn.addEventListener("click", (e) => {
 
-        // const taskName = document.createElement('input');
-        // taskName.type = 'text';
-        // taskName.classList.add('readonly');
-        // taskName.value = newTaskInput.value;
+        if(newTaskInput.value.length == 0)
+        {
+            alert("Please enter a task");
+        }else{
+            const task = document.createElement('div');
+            task.classList.add('task');
+            tasks.appendChild(task);
 
-        const taskName = document.createElement('p');
-        taskName.innerHTML = newTaskInput.value;
-        task.appendChild(taskName);
+            const taskName = document.createElement('input');
+            taskName.type = "text";
+            taskName.value = newTaskInput.value;
+            taskName.setAttribute('readonly', 'readonly');
+            task.appendChild(taskName);
+            newTaskInput.value = "";
 
-        const editLogo = document.createElement('i');
-        editLogo.classList.add('fa-solid');
-        editLogo.classList.add('fa-pen-to-square');
-        task.appendChild(editLogo);
+            const editLogo = document.createElement('i');
+            editLogo.classList.add('fa-solid');
+            editLogo.classList.add('fa-pen-to-square');
+            task.appendChild(editLogo);
+            
+            // EDIT TASK
+            editLogo.addEventListener("click", (e) => {
+                if (editLogo.classList == "fa-solid fa-pen-to-square") 
+                {
+                    editLogo.classList.remove("fa-pen-to-square");
+                    editLogo.classList.add("fa-floppy-disk");
+                    taskName.removeAttribute("readonly");
+                    taskName.focus();
+                } else {
+                    editLogo.classList.remove("fa-floppy-disk");
+                    editLogo.classList.add("fa-pen-to-square");
+                    taskName.setAttribute("readonly", "readonly");
+                }
+            });
 
+            // DELETE TASK
+            const deleteTask = document.createElement('i');
+            deleteTask.classList.add('fa-solid');
+            deleteTask.classList.add('fa-trash');
+            task.appendChild(deleteTask);
+            deleteTask.addEventListener("click", (e) => {
+                deleteTask.parentNode.remove();
 
-        editLogo.addEventListener("click", editTask);
-
-        // const editable = document.createElement('input');
-        // task.replaceChild(taskName, editable);
-    
-
-        const deleteTask = document.createElement('i');
-        deleteTask.classList.add('fa-solid');
-        deleteTask.classList.add('fa-trash');
-        task.appendChild(deleteTask);
-        // const checkbox = document.createElement('input');
-        // checkbox.type = 'checkbox';
-        // task.appendChild(checkbox); 
-        deleteTask.addEventListener("click", removeTask);
+            });
+        }
     }
-}
-
-function removeTask()
-{
-    this.parentNode.remove();
-}
-
-
-function editTask()
-{
-    edit.style.display = 'block';
-    edit.input.value = this.taskName.value;
-}
-
-
-
-pushBtn.addEventListener("click", addNewTask);
+    );
+});
